ec056b5aa9734f078f92cda24ce75296895c9ac7
piyoro <piyoro@naver.com>
2022-07-18 16:04:32 +0900

    ThreadLocal 변수 final 처리

--- src/main/java/com/piyoro/personalweb/common/util/ThreadUtil.java
+++ src/main/java/com/piyoro/personalweb/common/util/ThreadUtil.java
@@ -3,9 +3,9 @@ package com.piyoro.personalweb.common.util;
 import com.piyoro.personalweb.common.context.RequestContext;
 
 public class ThreadUtil {
-    private static ThreadLocal<String> tlReqTrcId = new ThreadLocal<>();
+    private final static ThreadLocal<String> tlReqTrcId = new ThreadLocal<>();
 
-    private static ThreadLocal<RequestContext> tlRequestContext = new ThreadLocal<>();
+    private final static ThreadLocal<RequestContext> tlRequestContext = new ThreadLocal<>();
 
     public static void setReqTrcId(String reqTrcId) {
         tlReqTrcId.set(reqTrcId);
